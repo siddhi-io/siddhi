cd5ade9d7d3f0641f91bdb1e8d9f93ba8a86245b
sayshika <seshika@wso2.com>
2014-06-27 17:36:43 +0530

    Committing Forecast and Outlier functionality

--- modules/siddhi-extensions/time-series/src/main/java/org/wso2/siddhi/extension/timeseries/linreg/SimpleLinearRegressionCalculator.java
+++ modules/siddhi-extensions/time-series/src/main/java/org/wso2/siddhi/extension/timeseries/linreg/SimpleLinearRegressionCalculator.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by waruna on 4/9/14.
+ * Created by seshika on 4/9/14.
  */
 public class SimpleLinearRegressionCalculator extends RegressionCalculator{
 
@@ -17,7 +17,7 @@ public class SimpleLinearRegressionCalculator extends RegressionCalculator{
     private List<Double> xValueList = new LinkedList<Double>();
     private List<Double> yValueList = new LinkedList<Double>();
     private int eventCount = 0;
-    private double sumX = 0.0, sumY = 0.0, sumxsquared = 0.0;
+    private double sumX = 0.0, sumY = 0.0, sumXsquared = 0.0;
     private double confidenceInterval = 0.0;
     private int batchSize = 1000000000;
     private int incCounter =0;
@@ -31,12 +31,12 @@ public class SimpleLinearRegressionCalculator extends RegressionCalculator{
     public void close () {
     }
 
-    public Object[] linearRegressionCalculation ( InEvent inEvent, Map<Integer, String> paramPositions, int paramCount, int limit, double ci ) {
-
+    public Object[] calculateLinearRegression(InEvent inEvent, Map<Integer, String> paramPositions, int paramCount, int calcInterval, int limit, double ci) {
 
         confidenceInterval = ci;
         batchSize = limit;
 
+        // TODO: implement this at the super class
         addEvent(inEvent, paramPositions, paramCount);
 
         // removing oldest events in order to maintain batchsize
@@ -45,10 +45,10 @@ public class SimpleLinearRegressionCalculator extends RegressionCalculator{
             removeEvent();
         }
 
-        //FOR PERFORMANCE TEST PURPOSES
-/*        if(incCounter%1000 != 0){
+        // processing at a user specified calculation interval
+        if(incCounter % calcInterval != 0){
             return null;
-        }*/
+        }
         return  processData();
     }
 
@@ -60,6 +60,7 @@ public class SimpleLinearRegressionCalculator extends RegressionCalculator{
         int itr = 0;
         double xValue = 0.0 , yValue = 0.0;
 
+        // TODO: Get rid of for loop, just get the 0th and 1st element
         for (Map.Entry<Integer, String> entry : paramPositions.entrySet()) {
 
             if (itr == 0) {
@@ -74,100 +75,83 @@ public class SimpleLinearRegressionCalculator extends RegressionCalculator{
         }
 
         sumX += xValue;
-        sumxsquared += Math.pow(xValue, 2);
+        sumXsquared += Math.pow(xValue, 2);
         sumY  += yValue;
     }
 
     public void removeEvent(){
 
-        sumX -= xValueList.get(0);
-        sumxsquared -= Math.pow(xValueList.get(0), 2);
-        sumY  -= yValueList.get(0);
+        double x = xValueList.remove(0);
 
-        xValueList.remove(0);
-        yValueList.remove(0);
+        sumX -= x;
+        sumXsquared -= Math.pow(x, 2);
+        sumY  -= yValueList.remove(0);
     }
 
-    public Object[] processData ()
-    {
+    public Object[] processData () {
 
-
-        Object[] dataObj = new Object[5];
-        try
-        {
+        Object[] regResult;
+        try {
+            // TODO: do a performance test on using linked list directly
             Double[] xArray = xValueList.toArray(new Double[eventCount]);
             Double[] yArray = yValueList.toArray(new Double[eventCount]);
 
-            double xbar, ybar, xxbar = 0.0, yybar = 0.0, xybar = 0.0, beta1, beta0, R2, stderr, beta1err, beta0err, t_beta0, t_beta1, fit;
+            double meanX, meanY, varianceX = 0.0, varianceY = 0.0, covarXY = 0.0, beta1, beta0, R2, stderr, beta1err, beta0err, t_beta0, t_beta1, fit;
 
             double resss = 0.0;      // residual sum of squares
-            double regss = 0.0;      // regression sum of squares
-            int df = eventCount - 2;      // degrees of freedom (n-k-1)
+            // double regss = 0.0;      // regression sum of squares [Required to calculate R2]
+            int df = eventCount - 2; // degrees of freedom (n-k-1)
             TDistribution t = new TDistribution(df);
 
-            //  compute xbar and ybar
-            xbar = sumX / eventCount;
-            ybar = sumY / eventCount;
+            //  compute meanX and meanY
+            meanX = sumX / eventCount;
+            meanY = sumY / eventCount;
 
             // compute summary statistics
-            for (int i = 0; i < eventCount; i++)
-            {
-                xxbar += (xArray[i] - xbar) * (xArray[i] - xbar);
-                yybar += (yArray[i] - ybar) * (yArray[i] - ybar);
-                xybar += (xArray[i] - xbar) * (yArray[i] - ybar);
+            for (int i = 0; i < eventCount; i++) {
+                // TODO: use pow method
+                varianceX += (xArray[i] - meanX) * (xArray[i] - meanX);
+                // varianceY += (yArray[i] - meanY) * (yArray[i] - meanY);  // Required to calculate R2
+                covarXY += (xArray[i] - meanX) * (yArray[i] - meanY);
             }
 
             //compute coefficients
-            beta1 = xybar / xxbar; // output param 1
-            beta0 = ybar - beta1 * xbar; // output param 2
+            beta1 = covarXY / varianceX;
+            beta0 = meanY - beta1 * meanX;
 
             // analyze results
-            for (int i = 0; i < eventCount; i++)
-            {
+            for (int i = 0; i < eventCount; i++) {
                 fit = beta1 * xArray[i] + beta0;
+                // TODO: use pow method
                 resss += (fit - yArray[i]) * (fit - yArray[i]);
-                regss += (fit - ybar) * (fit - ybar);
+                // regss += (fit - meanY) * (fit - meanY);  // Required to calculate R2
             }
 
-            //calculationg standard errors
-            R2 = regss / yybar;
-            stderr = Math.sqrt(resss / df); // output param 3
-            beta1err = stderr / Math.sqrt(xxbar);
-            beta0err = stderr * Math.sqrt(sumxsquared / (eventCount * xxbar));
+            // R2 = regss / varianceY;  // Will calculate only if a customer asks for it
+            //calculating standard errors
+            stderr = Math.sqrt(resss / df);
+            beta1err = stderr / Math.sqrt(varianceX);
+            beta0err = stderr * Math.sqrt(sumXsquared / (eventCount * varianceX));
 
             //calculating tstats
             t_beta0 = beta0 / beta0err;
             t_beta1 = beta1 / beta1err;
 
-            // Eliminating weak coefficiants
-            double pHighValue, pLowValue, pValue;
-
-            pHighValue = t.cumulativeProbability(Math.abs(t_beta0));
-            pLowValue = 1 - t.cumulativeProbability(Math.abs(t_beta0));
-            pValue = 1 - (pHighValue - pLowValue);
-
-            if (pValue > (1 - confidenceInterval))
-            {
+            // Eliminating weak coefficients
+            double pValue = 2 * (1-t.cumulativeProbability(Math.abs(t_beta0)));
+            if (pValue > (1 - confidenceInterval)) {
                 beta0 = 0;
             }
-
-            pHighValue = t.cumulativeProbability(Math.abs(t_beta1));
-            pLowValue = 1 - t.cumulativeProbability(Math.abs(t_beta1));
-            pValue = 1 - (pHighValue - pLowValue);
-
-            if (pValue > (1 - confidenceInterval))
-            {
+            pValue = 2 * (1-t.cumulativeProbability(Math.abs(t_beta1)));
+            if (pValue > (1 - confidenceInterval)) {
                 beta1 = 0;
             }
 
-            dataObj = new Object[]{stderr, beta0, beta1, t_beta0, t_beta1};
-
-            log.debug(dataObj);
-
-        } catch (Exception e)
-        {
+            regResult = new Object[]{stderr, beta0, beta1};
+        }
+        catch (Exception e) {
             return null;
         }
-        return dataObj;
+        return regResult;
     }
 }
