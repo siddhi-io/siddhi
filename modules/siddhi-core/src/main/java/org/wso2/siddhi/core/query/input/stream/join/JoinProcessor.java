eed0ce1044c96124716fb80095b88c5625843c03
ramindu90 <ramindus@wso2.com>
2016-01-17 21:32:47 +0530

    Adding formatting

--- modules/siddhi-core/src/main/java/org/wso2/siddhi/core/query/input/stream/join/JoinProcessor.java
+++ modules/siddhi-core/src/main/java/org/wso2/siddhi/core/query/input/stream/join/JoinProcessor.java
@@ -15,6 +15,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
+
 package org.wso2.siddhi.core.query.input.stream.join;
 
 
@@ -83,18 +84,17 @@ public class JoinProcessor implements Processor {
                     }
                 }
                 StreamEvent foundStreamEvent = findableProcessor.find(streamEvent, finder);
-                if(foundStreamEvent == null){
-                    if(outerJoinProcessor && !leftJoinProcessor) {
+                if (foundStreamEvent == null) {
+                    if (outerJoinProcessor && !leftJoinProcessor) {
                         returnEventChunk.add(joinBuilder(foundStreamEvent, streamEvent));
-                    }
-                    else if(outerJoinProcessor && leftJoinProcessor) {
+                    } else if (outerJoinProcessor && leftJoinProcessor) {
                         returnEventChunk.add(joinBuilder(streamEvent, foundStreamEvent));
                     }
-                }else{
+                } else {
                     while (foundStreamEvent != null) {
                         if (!leftJoinProcessor) {
                             returnEventChunk.add(joinBuilder(foundStreamEvent, streamEvent));
-                        }else{
+                        } else {
                             returnEventChunk.add(joinBuilder(streamEvent, foundStreamEvent));
                         }
                         foundStreamEvent = foundStreamEvent.getNext();
@@ -190,10 +190,11 @@ public class JoinProcessor implements Processor {
 
     /**
      * Join the given two event streams
-     * @param leftStream event left stream
+     *
+     * @param leftStream  event left stream
      * @param rightStream event right stream
      */
-    public StateEvent joinBuilder(StreamEvent leftStream,StreamEvent rightStream){
+    public StateEvent joinBuilder(StreamEvent leftStream, StreamEvent rightStream) {
         StateEvent returnEvent = stateEventPool.borrowEvent();
         returnEvent.setEvent(0, leftStream);
         returnEvent.setEvent(1, rightStream);
@@ -204,7 +205,7 @@ public class JoinProcessor implements Processor {
         }
         if (!leftJoinProcessor) {
             returnEvent.setTimestamp(rightStream.getTimestamp());
-        }else{
+        } else {
             returnEvent.setTimestamp(leftStream.getTimestamp());
         }
         return returnEvent;
