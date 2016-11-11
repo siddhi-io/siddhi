c16637d9d715372b34c739476ec4aa67a44d8c15
vithursa <vithu30@hotmail.com>
2016-11-11 16:32:50 +0530

    Moved two classes of AK-Slack to separate package.

---
+++ modules/siddhi-extensions/reorder/src/main/java/org/wso2/siddhi/extension/reorder/alphakslack/Runtime.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.siddhi.extension.reorder.alphakslack;
+
+import org.wso2.siddhi.core.exception.ExecutionPlanRuntimeException;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class Runtime{
+    private final long WINDOW_SIZE = 1000l;
+
+    /**
+     * Calculate Window Coverage
+     *
+     * @param eventTimeStamps
+     * @return
+     */
+    public double calculateWindowCoverage(LinkedHashSet<Long> eventTimeStamps) {
+        double windowCoverage = -1;
+        long count = 1;
+        int numerator = 0;
+        int denominator = 0;
+        long lowerIndex = 0;
+        long largestTimeStamp = -1;
+        long d = -1;
+        ArrayList timeStamps = new ArrayList();
+        timeStamps.addAll(eventTimeStamps);
+        Iterator<Long> itr;
+        itr = eventTimeStamps.iterator();
+        largestTimeStamp = getLargestTimeStamp(eventTimeStamps);
+
+        if(itr.hasNext()) {
+            d = (Long) itr.next();
+            long edgeValue = (largestTimeStamp - WINDOW_SIZE - 1);
+            long distance = Math.abs(d - edgeValue);
+
+            while (itr.hasNext()) {
+                long c = (Long) itr.next();
+                long cdistance = Math.abs(c - edgeValue);
+
+                if ((cdistance < distance) && (cdistance != 0)) {
+                    distance = cdistance;
+                    lowerIndex = (long) count;
+                }
+                count += 1;
+            }
+
+            try {
+                for (long i = edgeValue + 1; i <= largestTimeStamp - 1; i++) {
+                    if (eventTimeStamps.contains(i)) {
+                        int z = timeStamps.indexOf(i);
+                        int y = timeStamps.indexOf(largestTimeStamp);
+
+                        if ((z <= (y - 1)) && (z >= lowerIndex)) {
+                            numerator += 1;
+                        }
+                        denominator += 1;
+                    }
+                }
+                windowCoverage = numerator * 1.0 / denominator;
+            } catch (Exception e) {
+                throw new ExecutionPlanRuntimeException("Error in Window Coverage Calculation.", e);
+            }
+        }
+
+        return windowCoverage;
+    }
+
+    private long getLargestTimeStamp(LinkedHashSet<Long> eventTimeStamps){
+        Iterator<Long> itr = eventTimeStamps.iterator();
+        long largestItem = 0;
+        long item = 0;
+
+        while(itr.hasNext()){
+            item = itr.next();
+
+            if(item > largestItem){
+                largestItem = item;
+            }
+        }
+
+        return largestItem;
+    }
+}
---
+++ modules/siddhi-extensions/reorder/src/main/java/org/wso2/siddhi/extension/reorder/alphakslack/ThetaThreshold.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.siddhi.extension.reorder.alphakslack;
+
+import org.apache.commons.math3.distribution.NormalDistribution;
+
+import java.util.ArrayList;
+
+public class ThetaThreshold {
+    private double delta;
+    private double errorThreshold;
+    private long N;
+    private double idealMean;
+    private double idealVariance;
+
+    public ThetaThreshold(double errorThreshold, double delta) {
+        this.errorThreshold = errorThreshold;
+        this.delta = delta;
+    }
+
+    /**
+     * Calculation of CriticalValue
+     *
+     * @return
+     */
+    public double calculateCriticalValue() {
+        NormalDistribution actualDistribution = new NormalDistribution();
+        double criticalValue = Math.abs(actualDistribution.inverseCumulativeProbability(delta / 2));
+
+        return criticalValue;
+    }
+
+    public double calculateMean(ArrayList listOfEvents) {
+        N = listOfEvents.size();
+        long mean = 0;
+        for (int i = 0; i < listOfEvents.size(); i++) {
+            mean += (Long)listOfEvents.get(i);
+        }
+        idealMean = mean *1.0/ listOfEvents.size();
+        return idealMean;
+    }
+
+    public double calculateVariance(ArrayList listOfEvents) {
+        double variance = 0;
+        for (int i = 0; i < listOfEvents.size(); i++) {
+            variance += Math.pow(idealMean - (Long)listOfEvents.get(i), 2);
+        }
+        idealVariance = variance*1.0 / listOfEvents.size();
+        return idealVariance;
+    }
+
+    /**
+     * Solving inequality to get ThetaThreshold
+     *
+     * @param criticalValue
+     * @param idealMean
+     * @param idealVariance
+     * @return
+     */
+    public double calculateThetaThreshold(double criticalValue, double idealMean, double idealVariance) {
+        double temp1 = Math.sqrt((Math.pow(idealMean, 2) + Math.pow(idealVariance, 2)) / (N * Math.pow(idealMean, 2)));
+        double temp2 = Math.pow(criticalValue, 2) * Math.pow(temp1, 2);
+        double a1, b1, c1, b2, c2;
+        double thetaThresholdValue;
+        double theta1;
+        double theta2;
+        double theta3;
+        double theta4;
+        double tempSq1;
+        double tempSq2;
+
+        a1 = 1 + temp2;
+        b1 = (2 * errorThreshold) - 2 - temp2;
+        c1 = Math.pow((1 - errorThreshold), 2);
+        b2 = -(2 + (2 * errorThreshold) + temp2);
+        c2 = Math.pow((1 + errorThreshold), 2);
+        tempSq1 = Math.pow(b1, 2) - (4 * a1 * c1);
+        tempSq2 = Math.pow(b2, 2) - (4 * a1 * c2);
+
+        if (tempSq1 >= 0) {
+            theta1 = (-b1 - Math.sqrt(tempSq1)) / (2 * a1);
+            theta2 = theta1;
+        } else {
+            theta1 = theta2 = 0;
+        }
+
+        if (tempSq2 >= 0) {
+            theta3 = (-b2 - Math.sqrt(tempSq2)) / (2 * a1);
+            theta4 = theta3;
+        } else {
+            theta3 = theta4 = 0;
+        }
+
+
+        if ((tempSq1 >= 0) && (tempSq2 < 0)) {
+            thetaThresholdValue = Math.min(theta1, theta2);
+        } else if ((tempSq2 >= 0) && (tempSq1 < 0)) {
+            thetaThresholdValue = Math.min(theta3, theta4);
+        } else if ((tempSq1 < 0) && (tempSq2 < 0)) {
+            thetaThresholdValue = 0;
+        } else {
+            thetaThresholdValue = Math.min(Math.min(theta1, theta2), Math.min(theta3, theta4));
+        }
+
+        return thetaThresholdValue;
+    }
+}
