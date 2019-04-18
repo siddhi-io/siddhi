2b39cc24f964cda186f1c0fa027eba89284fd51c
Minudika Malshan <minudika@wso2.com>
2019-04-18 14:57:30 +0530

    Update modules/siddhi-core/src/main/java/io/siddhi/core/annotation/ProcessorAnnotationValidator.java
    Co-Authored-By: RolandHewage <44081958+RolandHewage@users.noreply.github.com>

--- modules/siddhi-core/src/main/java/io/siddhi/core/annotation/ProcessorAnnotationValidator.java
+++ modules/siddhi-core/src/main/java/io/siddhi/core/annotation/ProcessorAnnotationValidator.java
@@ -136,7 +136,7 @@ public class ProcessorAnnotationValidator {
                                     }
                                 }
                             }
-                            isCorrectOne = (found);
+                            isCorrectOne = found;
                         }
                     }
                     n = -1;
