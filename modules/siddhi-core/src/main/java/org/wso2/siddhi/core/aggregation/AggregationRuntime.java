31abdcb05f32f6c4f3ac672a5118ff5bc41c9fb3
Niveathika <niveathika@wso2.com>
2019-05-24 20:35:30 +0530

    Synchronise recreate in-memory method instead of function call

--- modules/siddhi-core/src/main/java/org/wso2/siddhi/core/aggregation/AggregationRuntime.java
+++ modules/siddhi-core/src/main/java/org/wso2/siddhi/core/aggregation/AggregationRuntime.java
@@ -431,7 +431,7 @@ public class AggregationRuntime implements MemoryCalculable {
         incrementalDataPurging.executeIncrementalDataPurging();
     }
 
-    public void recreateInMemoryData(boolean isFirstEventArrived) {
+    public synchronized void recreateInMemoryData(boolean isFirstEventArrived) {
         // State only updated when first event arrives to IncrementalAggregationProcessor
         if (isFirstEventArrived) {
             this.isFirstEventArrived = true;
@@ -440,9 +440,7 @@ public class AggregationRuntime implements MemoryCalculable {
                 durationIncrementalExecutorEntry.getValue().setProcessingExecutor(true);
             }
         }
-        synchronized (this) {
-            this.recreateInMemoryData.recreateInMemoryData();
-        }
+        this.recreateInMemoryData.recreateInMemoryData();
     }
 
     public void processEvents(ComplexEventChunk<StreamEvent> streamEventComplexEventChunk) {
