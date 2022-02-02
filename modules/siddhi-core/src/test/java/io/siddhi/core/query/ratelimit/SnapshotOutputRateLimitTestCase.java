4c9193b777d28110ddc3ff20085cc185188a6a42
dnwick <dnsnipe123@gmail.com>
2022-02-02 14:50:35 +0530

    Remove log4j

--- modules/siddhi-core/src/test/java/io/siddhi/core/query/ratelimit/SnapshotOutputRateLimitTestCase.java
+++ modules/siddhi-core/src/test/java/io/siddhi/core/query/ratelimit/SnapshotOutputRateLimitTestCase.java
@@ -27,7 +27,7 @@ import io.siddhi.core.stream.output.StreamCallback;
 import io.siddhi.core.util.EventPrinter;
 import io.siddhi.core.util.SiddhiTestHelper;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Logger;
 import org.testng.Assert;
 import org.testng.AssertJUnit;
 import org.testng.annotations.BeforeMethod;
@@ -36,7 +36,7 @@ import org.testng.annotations.Test;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SnapshotOutputRateLimitTestCase {
-    private static final Logger log = LogManager.getLogger(SnapshotOutputRateLimitTestCase.class);
+    private static final Logger log = (Logger) LogManager.getLogger(SnapshotOutputRateLimitTestCase.class);
     private volatile AtomicInteger count;
     private long value;
     private volatile boolean eventArrived;
